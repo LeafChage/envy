2180e3c70013dae0600414c5a71ad126310b2476
LeafChage <t.kusanagi5512@gmail.com>
2023-07-21 13:57:46 +0900

    ok

--- src/envy/load.rs
+++ src/envy/load.rs
@@ -1,11 +1,16 @@
 use std::path::PathBuf;
 use std::process::Command;
 
-pub fn action(path: &PathBuf, cmd: &Vec<String>) -> Result<(), anyhow::Error> {
-    let envs = super::env::parser_ignore_meta(path)?
-        .into_iter()
-        .map(|env| env.tuple())
-        .collect::<Vec<_>>();
+pub fn action(paths: &Vec<PathBuf>, cmd: &Vec<String>) -> Result<(), anyhow::Error> {
+    let mut envs = vec![];
+    for path in paths.into_iter() {
+        envs.append(
+            &mut super::env::parser_ignore_meta(path)?
+                .into_iter()
+                .map(|env| env.tuple())
+                .collect::<Vec<_>>(),
+        );
+    }
 
     if cmd.len() == 0 {
         return Err(anyhow::Error::msg("cmd is empty"));
--- src/main.rs
+++ src/main.rs
@@ -24,31 +24,31 @@ struct Cli {
 enum Commands {
     /// convert to json
     Json {
-        #[arg(short = 'f', long)]
-        input: PathBuf,
+        #[arg(value_name = "ENV_FILE")]
+        path: PathBuf,
     },
-    /// load dotenv file
+    /// load dotenv file and run command
     Load {
-        #[arg(short = 'f', long)]
-        input: PathBuf,
+        #[arg(short='f',num_args = 1.., value_delimiter = ' ', value_name="ENV_FILE")]
+        input: Vec<PathBuf>,
 
-        #[arg(long, allow_hyphen_values = true, num_args = 1.., value_delimiter = ' ')]
+        #[arg(long = "", allow_hyphen_values = true, num_args = 1.., value_delimiter = ' ', value_name="CMD|ARGS")]
         cmd: Vec<String>,
     },
     /// generate key
     Key {},
     /// encrypt dotenv file
     Encrypt {
-        #[arg(short = 'f', long, value_name = "FILE")]
-        input: PathBuf,
+        #[arg(short = 'f', value_name = "ENV_FILE")]
+        path: PathBuf,
 
         #[arg(short = 'k', long, value_name = "BASE64")]
         key: String,
     },
     /// decrypt dotenv file
     Decrypt {
-        #[arg(short = 'f', long, value_name = "FILE")]
-        input: PathBuf,
+        #[arg(short = 'f', value_name = "ENV_FILE")]
+        path: PathBuf,
 
         #[arg(short = 'k', long, value_name = "BASE64")]
         key: String,
@@ -58,11 +58,11 @@ enum Commands {
 fn main() -> Result<(), anyhow::Error> {
     let cli = Cli::parse();
     match &cli.command {
-        Some(Commands::Json { input }) => envy::json::action(input),
+        Some(Commands::Json { path }) => envy::json::action(path),
         Some(Commands::Load { input, cmd }) => envy::load::action(input, cmd),
         Some(Commands::Key {}) => envy::key::action(),
-        Some(Commands::Encrypt { input, key }) => envy::encrypt::action(input, key),
-        Some(Commands::Decrypt { input, key }) => envy::decrypt::action(input, key),
+        Some(Commands::Encrypt { path, key }) => envy::encrypt::action(path, key),
+        Some(Commands::Decrypt { path, key }) => envy::decrypt::action(path, key),
         None => Err(anyhow::Error::msg("unexpected subcommand")),
     }
 }
